27ebdc6f5323a971efe9ffceea5bb03b612a6965
kduong <katherineduong12@gmail.com>
2022-06-08 13:51:36 +1000

    updated model files

--- client/js/components/add_specs.js
+++ client/js/components/add_specs.js
@@ -2,8 +2,57 @@
 
 // Add CPUs
 
-function renderAddCPUs() {
-  document.querySelector('').innerHTML = `
-  
+function renderAddSpecs() {
+  document.querySelector('#page').innerHTML = `
+  <section class="select-specs">
+    <form onSubmit="createSpecs(event)">
+        <h2>Select your specs</h2>
+        <fieldset>
+            <label for="">CPUs</label>
+            <option class="cpu_list">
+                ${renderCPUList()}
+            </option>
+        </fieldset>
+
+        <fieldset>
+            <label for="">Graphics Cards</label>
+            
+        </fieldset>
+
+        <fieldset>
+            <label for="">Ram</label>
+            
+        </fieldset>
+
+        <fieldset>
+            <label for="">Motherboards</label>
+            
+        </fieldset>
+
+        <fieldset>
+            <label for="">SSDs</label>
+            
+        </fieldset>
+
+        <fieldset>
+            <label for="">PSUs</label>
+            
+        </fieldset>
+
+        <fieldset>
+            <label for="">Cases</label>
+            
+        </fieldset>
+    </form>
+  </section>
   `
 }
+
+function renderCPUList() {
+  
+}
+
+function createSpecs(event) {
+
+}
+
