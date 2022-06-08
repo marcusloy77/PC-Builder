ef2bca9384d84d43e5fcee8f682bdfe7451d6cf2
kduong <katherineduong12@gmail.com>
2022-06-08 11:03:43 +1000

    committing before pulling down recent changes

---
+++ client/js/components/add_specs.js
@@ -0,0 +1,9 @@
+// Include if statement - if the user selects a PC then they will be able to add specs:
+
+// Add CPUs
+
+function renderAddCPUs() {
+  document.querySelector('').innerHTML = `
+  
+  `
+}
